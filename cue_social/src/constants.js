f783467f41c253000d073029affbb2ed58172fe8
Jake Sanghavi <jake.sanghavi@gmail.com>
2024-07-09 14:01:34 -0400

    Update routing for prod

--- cue_social/src/constants.js
+++ cue_social/src/constants.js
@@ -1,15 +1,15 @@
 // function to set a constant ()
 function define(name, value) {
-    Object.defineProperty(exports, name, {
-      value: value,
-      enumerable: true
-    });
-  }
-  
-  
-  // For Development:
-  define("ORIGIN", 'http://localhost:3000');
-  define("RANDOM_SONG", 'http://localhost:3008/api/songs/random/random/');
-  define("DAILY_SONG",'http://localhost:3008/api/dailysong/');
-  define("ALL_SONGS", 'http://localhost:3008/api/songs/');
-  define("ROUTE", 'http://localhost:3008');
+  Object.defineProperty(exports, name, {
+    value: value,
+    enumerable: true
+  });
+}
+
+// For Prod:
+define("ORIGIN", 'https://cuetavern.netlify.app');
+define("ROUTE", 'https://cuetavern.onrender.com');
+
+// For Development:
+// define("ORIGIN", 'http://localhost:3000');
+// define("ROUTE", 'http://localhost:3008');
