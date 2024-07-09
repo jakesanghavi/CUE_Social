245af2591abb6525a53c09059d7f1397a80161f8
Jake Sanghavi <jake.sanghavi@gmail.com>
2024-07-09 00:54:19 -0400

    Make upload form a modal on home page

--- cue_social/src/pages/Home.js
+++ cue_social/src/pages/Home.js
@@ -1,14 +1,35 @@
+import React, { useState } from 'react';
 import UploadForm from '../components/UploadForm';
 import Login from '../components/Login';
 import '../component_styles/home.css';
 
-
 const Home = ({ loggedInUser, onLoginSuccess, uid }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="Home" id="home">
       <Login onLoginSuccess={onLoginSuccess} uid={uid} />
-      <UploadForm loggedInUser={loggedInUser} />
+      {loggedInUser && loggedInUser.email && (
+        <button onClick={openModal}>
+          Upload a deck
+        </button>
+      )}
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <button className="modal-close" onClick={closeModal}>Close</button>
+            <UploadForm loggedInUser={loggedInUser} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
