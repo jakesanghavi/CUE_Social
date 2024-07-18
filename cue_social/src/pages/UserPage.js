6159b9eb7f716586db9623171695c8e8c29fe5c4
Jake Sanghavi <jake.sanghavi@gmail.com>
2024-07-17 21:47:37 -0400

    User search now populates with the user's profile

--- cue_social/src/pages/UserPage.js
+++ cue_social/src/pages/UserPage.js
@@ -1,30 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import { ROUTE } from '../constants';
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 const UserPage = () => {
   const { userId } = useParams();
+  const [decks, setDecks] = useState([]);
+
+  const fetchDecksForUser = useCallback(async (user) => {
+    if (user) {
+      try {
+        const response = await fetch(`${ROUTE}/api/decks/${user}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch decks');
+        }
+        const decksData = await response.json();
+        setDecks(decksData);
+      } catch (error) {
+        console.error('Error fetching decks:', error);
+      }
+    }
+  }, []);
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch(`${ROUTE}/api/users/getone/${userId}`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch user');
-        }
-        const deckData = await response.json();
-      } catch (error) {
-        console.error('Error fetching user:', error);
-      }
-    };
-
-    fetchUser();
-  }, [userId]);
+    fetchDecksForUser(userId);
+  }, [userId, fetchDecksForUser]);
 
   return (
     <div>
-      <h2>{userId}</h2>
+      <div>
+        {userId ? <div>Username: {userId}</div> : null}
+      </div>
+      <div>
+        <h2>Decks</h2>
+        <div className="grid-container">
+          {decks.map(deck => (
+            <div key={deck._id} className="grid-item">
+              <Link to={`/decks/${deck._id}`}>
+                <div>Title: {deck.title}</div>
+                <div>Description: {deck.description}</div>
+                {deck.image && (
+                  <img
+                    src={`data:image/jpeg;base64,${Buffer.from(deck.image.data).toString('base64')}`}
+                    alt="Decklist"
+                  />
+                )}
+              </Link>
+              <hr />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
