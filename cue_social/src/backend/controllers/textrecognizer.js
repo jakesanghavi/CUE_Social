534c544e81eb39ff5a3c860d614eb8d7454deeeb
Jake Sanghavi <jake.sanghavi@gmail.com>
2024-07-19 09:38:05 -0400

    OCR API for speed

--- cue_social/src/backend/controllers/textrecognizer.js
+++ cue_social/src/backend/controllers/textrecognizer.js
@@ -1,244 +1,118 @@
-const { createWorker } = require('tesseract.js');
 const sharp = require('sharp');
 const path = require('path')
 const os = require('os')
+const { ocrSpace } = require('ocr-space-api-wrapper');
 
 const recognizeText = async (request, response) => {
     const acceptChars = ['(', ' ', ')', '{', '}'];
+    const file = request.file;
+    let deckCode = "";
 
-    const processLines = (lineString) => {
-        const cards = [];
+    const compressedImage = await sharp(file.buffer) // Input image buffer
+        .resize({ width: 1000 }) // Resize the image to a width of 800px (adjust as needed)
+        .webp({ quality: 100 })
+        .toBuffer() // Convert to JPEG with 50% quality (adjust as needed)
 
-        const lines = lineString.split('\n');
+    const sizeInBytes = compressedImage.length;
+    console.log(`Size: ${sizeInBytes} bytes`);
 
-        lines.forEach(line => {
-            if (line.includes("DECK CODE:")) {
-                const codes = line.split(":");
-                const code = codes[codes.length - 1];
-                const deckCode = code.trim();
-            }
-
-            line = line.split(' ');
-            let i = 0;
-            let flag = false;
-
-            while (!flag && i < line.length) {
-                if (line[i] === line[i].toUpperCase()) {
-                    flag = true;
-                } else {
-                    i += 1;
-                }
-            }
-
-            line = line.slice(i);
-            line = line.join(' ');
-
-            let indexOfFirstCapital = null;
-
-            for (let index = 0; index < line.length; index++) {
-                const char = line[index];
-                if (char === char.toUpperCase()) {
-                    indexOfFirstCapital = index;
-                    break;
-                }
-            }
-
-            let leadingLine = [];
-            if (indexOfFirstCapital !== null) {
-                leadingLine = line.slice(indexOfFirstCapital);
-            }
-
-            let trailingLine = [];
-            let foundLower = false;
-            for (const char of leadingLine) {
-                if (char.toLowerCase() === char || (acceptChars.indexOf(char) === -1 && char === char.toLowerCase())) {
-                    foundLower = true;
-                    break;
-                }
-                if (!foundLower) {
-                    trailingLine.push(char);
-                }
-            }
-
-            trailingLine = trailingLine.join('');
-
-            const capitalLetters = trailingLine.split('').filter(char => char === char.toUpperCase() || acceptChars.includes(char)).join('').trim();
-
-            // Remove single-character words unless they are 'A' or 'I'
-            const processedLineWords = [];
-            capitalLetters.split(' ').forEach(word => {
-                if (word.length > 1 || word === 'A' || word === 'I') {
-                    processedLineWords.push(word);
-                }
-            });
-
-            const processedLine = processedLineWords.join(' ').trim();
-            if (processedLine.length > 0) {
-                cards.push(processedLine);
-            }
-        });
-
-        return cards;
-    };
+    const base64Image = compressedImage.toString('base64');
 
     try {
-        const file = request.file
-        const tempDir = os.tmpdir();
-        const filePath = path.join(tempDir, file.originalname);
+        const res3 = await ocrSpace(`data:image/webp;base64,${base64Image}`, { apiKey: process.env.OCR_KEY });
+        const cardsReturned = res3.ParsedResults[0].ParsedText
+        const linesArray = cardsReturned.split(/\r?\n/);
 
-        await sharp(file.buffer).toFile(filePath);
+        const processLines = (lines) => {
+            const cards = [];
 
-        const worker = await createWorker("eng");
+            lines.forEach((line) => {
+                // console.log(`Line: ${line}`);
 
-        const ret = await worker.recognize(filePath);
-        const text = ret.data.text;
-        // console.log(ret.data.hocr)
-        // const toReturn = processLines(text)
-        // await worker.terminate();
-
-        const wordRegex = /<span class='ocrx_word'[^>]*title='bbox (\d+) (\d+) (\d+) (\d+); x_wconf (\d+)'[^>]*>([^<]+)<\/span>/g;
-        let match;
-        const words = [];
-
-
-        while ((match = wordRegex.exec(ret.data.hocr)) !== null) {
-            const [_, x1, y1, x2, y2, x_wconf, text] = match;
-            if (x_wconf > 30) {
-                words.push({
-                    text,
-                    bbox: {
-                        x1: parseInt(x1),
-                        y1: parseInt(y1),
-                        x2: parseInt(x2),
-                        y2: parseInt(y2),
-                    },
-                    x_wconf: parseInt(x_wconf)
-                });
-            }
-        }
-
-        // console.log(words);
-
-        const threshold = 40; // Adjust this value based on your specific needs
-
-        const groupedWords = [];
-        let currentGroup = [];
-        let cards = [];
-        let deckCode = "";
-
-        for (let i = 0; i < words.length; i++) {
-            const currentWord = words[i];
-            if (currentGroup.length === 0) {
-                currentGroup.push(currentWord);
-            } else {
-                const lastWord = currentGroup[currentGroup.length - 1];
-                const distance = currentWord.bbox.x1 - lastWord.bbox.x2;
-
-                if (distance <= threshold) {
-                    currentGroup.push(currentWord);
-                } else {
-                    groupedWords.push(currentGroup);
-                    currentGroup = [currentWord];
+                if (line.includes("DECK CODE:")) {
+                    let codes = line.split(":");
+                    let oneCode = codes[codes.length - 1].split(' ')
+                    let code = null
+                    for (let k = 0; k < oneCode.length; k++) {
+                        if (oneCode[k].length === 6) {
+                            code = oneCode[k]
+                        }
+                    }
+                    deckCode = code.trim();
                 }
-            }
-        }
 
-        if (currentGroup.length > 0) {
-            groupedWords.push(currentGroup);
-        }
+                line = line.split(' ');
+                let i = 0;
+                let flag = false;
 
-
-        const phrases = groupedWords.map(group => group.map(word => word.text).join(' '));
-        const acceptChars = ['(', ' ', ')', '{', '}'];
-
-        // console.log("Extracted and separated text:");
-        phrases.forEach((line) => {
-            // console.log(`Line: ${line}`);
-
-            if (line.includes("DECK CODE:")) {
-                let codes = line.split(":");
-                let oneCode = codes[codes.length - 1].split(' ')
-                let code = null
-                for (let k = 0; k < oneCode.length; k++) {
-                    if (oneCode[k].length === 6) {
-                        code = oneCode[k]
+                while (!flag && i < line.length) {
+                    if (line[i].toUpperCase() === line[i]) {
+                        flag = true;
+                    } else {
+                        i++;
                     }
                 }
-                // let code = codes[codes.length - 1];
-                deckCode = code.trim();
-                // console.log(deckCode)
-            }
 
-            line = line.split(' ');
-            let i = 0;
-            let flag = false;
+                line = line.slice(i).join(' ');
 
-            while (!flag && i < line.length) {
-                if (line[i].toUpperCase() === line[i]) {
-                    flag = true;
-                } else {
-                    i++;
+                let indexOfFirstCapital = null;
+                for (let index = 0; index < line.length; index++) {
+                    const char = line[index];
+                    // Check if the character is an uppercase letter or a quote
+                    if ((char >= 'A' && char <= 'Z') || char === '"' || char === "'") {
+                        indexOfFirstCapital = index;
+                        break;
+                    }
                 }
-            }
 
-            line = line.slice(i).join(' ');
-
-            let indexOfFirstCapital = null;
-            for (let index = 0; index < line.length; index++) {
-                const char = line[index];
-                // Check if the character is an uppercase letter or a quote
-                if ((char >= 'A' && char <= 'Z') || char === '"' || char === "'") {
-                    indexOfFirstCapital = index;
-                    break;
+                let leadingLine = [];
+                if (indexOfFirstCapital !== null) {
+                    leadingLine = line.slice(indexOfFirstCapital);
                 }
-            }
 
-            let leadingLine = [];
-            if (indexOfFirstCapital !== null) {
-                leadingLine = line.slice(indexOfFirstCapital);
-            }
+                // console.log(leadingLine)
 
-            // console.log(leadingLine)
-
-            let trailingLine = [];
-            let foundLower = false;
-            for (let char of leadingLine) {
-                if (char >= 'a' && char <= 'z' || (!acceptChars.includes(char) && !(char >= 'A' && char <= 'Z'))) {
-                    foundLower = true;
-                    break;
+                let trailingLine = [];
+                let foundLower = false;
+                for (let char of leadingLine) {
+                    if (char >= 'a' && char <= 'z' || (!acceptChars.includes(char) && !(char >= 'A' && char <= 'Z'))) {
+                        foundLower = true;
+                        break;
+                    }
+                    if (!foundLower) {
+                        trailingLine.push(char);
+                    }
                 }
-                if (!foundLower) {
-                    trailingLine.push(char);
+
+                trailingLine = trailingLine.join('');
+                // console.log(`Trailing Line: ${trailingLine}`);
+
+                let capitalLetters = [...trailingLine].filter(char => char.toUpperCase() === char || acceptChars.includes(char)).join('').trim();
+                // console.log(`Capital Letters: ${capitalLetters}`);
+
+                let processedLineWords = [];
+                capitalLetters.split(' ').forEach((word) => {
+                    if (word.length > 1 || ['A', 'I'].includes(word)) {
+                        processedLineWords.push(word);
+                    }
+                });
+
+                let processedLine = processedLineWords.join(' ').trim();
+                if (processedLine.length > 0) {
+                    cards.push(processedLine);
                 }
-            }
-
-            trailingLine = trailingLine.join('');
-            // console.log(`Trailing Line: ${trailingLine}`);
-
-            let capitalLetters = [...trailingLine].filter(char => char.toUpperCase() === char || acceptChars.includes(char)).join('').trim();
-            // console.log(`Capital Letters: ${capitalLetters}`);
-
-            let processedLineWords = [];
-            capitalLetters.split(' ').forEach((word) => {
-                if (word.length > 1 || ['A', 'I'].includes(word)) {
-                    processedLineWords.push(word);
-                }
+                // console.log(`Processed Letters: ${processedLine}`);
             });
-
-            let processedLine = processedLineWords.join(' ').trim();
-            if (processedLine.length > 0) {
-                cards.push(processedLine);
-            }
-            // console.log(`Processed Letters: ${processedLine}`);
-        });
-
-        const resp = {
-            cards: cards,
-            deck_code: deckCode
+            return cards;
         };
 
-        console.log(resp)
+        const toReturn = processLines(linesArray);
 
+        const resp = {
+            cards: toReturn,
+            deck_code: deckCode
+        };
+    
         return response.status(200).json(resp);
     }
     catch (error) {
